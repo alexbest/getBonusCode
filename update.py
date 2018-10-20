47facbe06713bd7ad7dc3cdcb1f689c55c163aa5
WrBug <i@wrbug.com>
2018-10-20 15:41:56 +0800

    Update update.py

--- update.py
+++ update.py
@@ -9,6 +9,7 @@ def check_update():
     print('币合区块链博纳云抢码脚本,版本号：' + local_version)
     print('使用时请同步好本地时间')
     print('使用后面的邀请地址注册，更容易成功，博纳云注册：https://console.bonuscloud.io/signUp?refer=264a1ce0d14511e894a05731b778d621')
+    print('软件使用问题请查看：https://bbs.bihe.one/thread-1445.htm')
     print('更多工具请关注：币合区块链')
     print('币合官方群：490389116')
     try:
